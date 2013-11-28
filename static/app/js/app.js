7e6fa687ea51e14bdbe6950ac200591f3055b2a1
Andrey Kaygorodov <info@kaygorodov.com>
2013-11-28 16:19:42 +0800

    integrated the light-blue library

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -10,7 +10,7 @@ angular.module('millibApp', [
   'millibApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: '/app/partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: '/app/partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
