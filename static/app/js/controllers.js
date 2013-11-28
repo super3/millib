92a2589c10a29b910c8b8c41ac3c2e7a29018760
Andrey Kaygorodov <info@kaygorodov.com>
2013-11-29 03:49:27 +0800

    changed chart size, added legends for series, added function for time format

--- static/app/js/controllers.js
+++ static/app/js/controllers.js
@@ -45,26 +45,19 @@ angular.module('millibApp.controllers', []).
             var newChartData = angular.copy($scope.chartData);
             angular.forEach(value, function(item) {
                 angular.forEach(chartLines, function(line, place) {
-                    var val = [item.date, item[line]];
+                    var val = [item.ts, item[line]];
                     newChartData[place].values.push(val);
                 });
             });
             $scope.chartData = newChartData;
         });
 
-        $scope.xAxisTicksFunction = function(){
-            console.log('xAxisTicksFunction');
-            console.log(d3.svg.axis().ticks(d3.time.minutes, 5));
+        $scope.xAxisTickFormatFunction = function(){
             return function(d){
-                return d3.svg.axis().ticks(d3.time.minutes, 5);
+                return d3.time.format('%m/%d/%y %H:%M')(moment.unix(d).toDate());
             }
         };
 
-        $scope.xAxisTickFormatFunction = function(){
-            return function(d){
-                return d3.time.format('%H:%M')(moment.unix(d).toDate());
-            }
-        };
     }])
   .controller('AboutCtrl', [function() {
 
